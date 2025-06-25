544029dd80d26c14673b22a003be4420d8a9276b
Vladyslav Kasatonov <vld.kasatonov@gmail.com>
2025-06-25 20:20:54 +0300

    add values for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	double C = 230;
+	int N = 5;
+	int K = 8;
+	std::vector<double> price = { 150, 220, 300, 180, 450, 270, 380, 120 };
+	std::vector<double> gains = { 50, 50, 100, 40, 100, 80, 70, 40 };
+
 	return 0;
 }
